0b66bd3af23fc5130fbe520bd68a5a24b92a1376
Bryce Reading <66621652+BryceReading@users.noreply.github.com>
2022-01-05 23:01:30 +0000

    Mouse control classes but not fully working

--- DirectX_11_Project/Mouse/MouseClass.h
+++ DirectX_11_Project/Mouse/MouseClass.h
@@ -1,1 +1,36 @@
 #pragma once
+#include "MouseEvent.h"
+#include <queue>
+using namespace std;
+
+class MouseClass
+{
+public:
+	void onLPress(int x, int y);
+	void onLRelease(int x, int y);
+	void onRPress(int x, int y);
+	void onRRelease (int x, int y);
+	void onMPress(int x, int y);
+	void onMRelease (int x, int y);
+	void onWheelUp(int x, int y);
+	void onWheelDown(int x, int y);
+
+	bool leftDown();
+	bool midDown();
+	bool rightDown();
+
+	int xPos();
+	int yPos();
+	MousePoint getPosition();
+
+	bool bufferEventEmpty();
+	MouseEvent eventRead();
+
+private:
+	queue<MouseEvent> bufferEvent;
+	bool leftDown = false;
+	bool rightDown = false; 
+	bool midMouseDown = false;
+	int x = 0;
+	int y = 0;
+};
--- DirectX_11_Project/Mouse/MouseEvent.cpp
+++ DirectX_11_Project/Mouse/MouseEvent.cpp
@@ -1,3 +1,42 @@
 #include "MouseEvent.h"
+#include "..\MouseEvent.h"
 
-//MouseEvent::MouseEvent() : type(EventType::Invalid), x(0), y(0) {}
+MouseEvent::MouseEvent()
+	:
+	type(EventType::Invalid),
+	x(0),
+	y(0)
+{}
+
+MouseEvent::MouseEvent(EventType t, int x, int y)
+	:
+	type(t),
+	x(x),
+	y(y)
+{}
+
+bool MouseEvent::IsValid() const
+{
+	return this->type != EventType::Invalid;
+}
+
+MouseEvent::EventType MouseEvent::GetType() const
+{
+	return this->type;
+}
+
+MousePoint MouseEvent::GetPos() const
+{
+	return { this->x, this->y };
+}
+
+int MouseEvent::GetPosX() const
+{
+	return this->x;
+}
+
+int MouseEvent::GetPosY() const
+{
+	return this->y;
+}
+
