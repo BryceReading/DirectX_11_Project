6565fdd0bb2b45c13c9ec9c1830636d8c9276e9e
Bryce Reading <66621652+BryceReading@users.noreply.github.com>
2022-02-19 11:45:37 +0000

    Mouse Controls

---
+++ DirectX_11_Project/Mouse/MouseClass.cpp
@@ -0,0 +1,38 @@
+#include "MouseClass.h"
+
+//void MouseClass::onLPress(int x, int y)
+//{
+//	//this->leftDown = true;
+//	//MouseEvent m(MouseEvent::EventType::LPress, x, y);
+//	//this->bufferEvent.push(m);
+//}
+//
+//void MouseClass::onLRelease(int x, int y)
+//{
+//	//this->leftDown = false;
+//	//this->bufferEvent.push(MouseEvent(MouseEvent::EventType::LRelease, x, y));
+//}
+//
+//void MouseClass::onRPress(int x, int y)
+//{
+//}
+//
+//void MouseClass::onRRelease(int x, int y)
+//{
+//}
+//
+//void MouseClass::onMPress(int x, int y)
+//{
+//}
+//
+//void MouseClass::onMRelease(int x, int y)
+//{
+//}
+//
+//void MouseClass::onWheelUp(int x, int y)
+//{
+//}
+//
+//void MouseClass::onWheelDown(int x, int y)
+//{
+//}
